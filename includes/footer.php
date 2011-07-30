940bef8c4dcef6dc450c442ed5dcefb81baf9d8e
ermmannning <erm.mannning@gmail.com>
2011-07-30 11:51:26 -0400

    added js to footer and header

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,9 @@
 <footer>
+	<img src="logo.png"/>
 	<p>Something Else!</p>
 </footer>
+
+<script type="text/javascript" src="" ></script>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
+
 	require_once("includes/header.php");
 
-	
-	
 	require_once("includes/footer.php");
 ?>
